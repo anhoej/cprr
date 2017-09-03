69e8044b04044743981e3eeaea4fd82a2a134d0a
anhoej <jacob@anhoej.net>
2017-09-03 15:14:25 +0200

    Improve error checking of cpr numbers and allow for hyphen between dob and sequence number.

--- R/cprr.R
+++ R/cprr.R
@@ -1,30 +1,8 @@
-#' Scramble
-#'
-#' Create data frame translating CPR numbers to random numbers.
-#'
-#' @param cpr Character vector of CPR numbers without hyphens (10 digits).
-#'
-#' @return A data frame with corresponding CPR and anonymous id numbers.
-#'
-#' @examples
-#'   scramble(c('1508631111', '1310762222'))
-#'
-#' @export
-#'
-scramble <- function(cpr) {
-  x <- seq_along(sample(unique(cpr)))
-  names(x) <- sample(unique(cpr))
-  x <- x[cpr]
-  names(x) <- NULL
-  x
-# scramble(c('1508631111', '1310762222', '1310762222', '1508631111'))
-}
-
 #' Date of birth
 #'
-#' Calculate date of birth from CPR number.
+#' Calculate dates of birth from CPR numbers.
 #'
-#' @param cpr Character Vector of CPR numbers without hyphens (10 digits).
+#' @param cpr Character Vector of CPR numbers with or without hyphens.
 #'
 #' @return Vector of dates of birth.
 #'
@@ -34,42 +12,68 @@ scramble <- function(cpr) {
 #' @export
 #'
 dob <- function(cpr) {
-  if(any(nchar(cpr) != 10)) {
-    stop('One or more invalid CPR numbers.')
-  }
+  cpr <- clean(cpr)
 
-  d <- lapply(cpr, dobone)
+  # d <- lapply(cpr, dobone)
+  d <- lapply(cpr, function(cpr) {
+    dd <- as.numeric(substr(cpr, 1, 2))
+    mm <- as.numeric(substr(cpr, 3, 4))
+    yy <- as.numeric(substr(cpr, 5, 6))
+    x7 <- as.numeric(substr(cpr, 7, 7))
+
+    if(is.na(cpr)) {
+      NA
+    } else {
+      if(x7 < 4) {
+        yyyy <- 1900
+      } else if(x7 == 4 & yy < 37) {
+        yyyy <- 2000
+      } else if(x7 == 4) {
+        yyyy <- 1900
+      } else if(x7 == 5 & yy < 58) {
+        yyyy <- 2000
+      } else if(x7 == 5) {
+        yyyy <- 1800
+      } else if(x7 == 6 & yy < 58) {
+        yyyy <- 2000
+      } else if(x7 == 6) {
+        yyyy <- 1800
+      } else if(x7 == 7 & yy < 58) {
+        yyyy <- 2000
+      } else if(x7 == 7) {
+        yyyy <- 1800
+      } else if(x7 == 8 & yy < 58) {
+        yyyy <- 2000
+      } else if(x7 == 8) {
+        yyyy <- 1800
+      } else if(x7 == 9 & yy < 37) {
+        yyyy <- 2000
+      } else {
+        yyyy <- 1900
+      }
+
+      yyyy <- yyyy + yy
+      dob <- paste(yyyy, mm, dd, sep = '-')
+      dob <- tryCatch(as.Date(dob),
+                      error = function(e) NA)
+      if(is.na(dob)) {
+        warning(paste(cpr, 'is not a valid CPR number. NA returned.'),
+                call. = FALSE)
+      }
+
+      return(dob)
+    }
+  })
+
   d <- unlist(d)
   as.Date(d, origin = '1970-01-01')
 }
 
-#' Gender
-#'
-#' Calculate gender from CPR number.
-#'
-#' @param cpr Character Vector of CPR numbers without hyphen (10 digits).
-#'
-#' @return Integer vector of genders. 0 = Female, 1 = Male.
-#'
-#' @examples
-#'   gender(c('1508631111', '1310762222'))
-#'
-#' @export
-#'
-gender <- function(cpr) {
-  if(any(nchar(cpr) != 10)) {
-    stop('One or more invalid CPR numbers.')
-  }
-
-  as.numeric(substring(cpr, 10)) %% 2
-
-}
-
 #' Age
 #'
-#' Calculate age from CPR number
+#' Calculate ages from CPR numbers.
 #'
-#' @param cpr Character Vector of CPR numbers without hyphen (10 digits).
+#' @param cpr Character Vector of CPR numbers with or without hyphens.
 #' @param date Character or date vector of dates used in computation of ages.
 #'   Dates provided as characters must follow the ISO standard, 'yyyy-mm-dd'.
 #' @param unit Character indicating the unit representing ages. Possible values
@@ -85,56 +89,66 @@ gender <- function(cpr) {
 #' @export
 #'
 age <- function(cpr, date = Sys.Date(), unit = 'year') {
-  if(any(nchar(cpr) != 10)) {
-    stop('One or more invalid CPR numbers.')
-  }
+  cpr <- clean(cpr)
 
   u <- switch(unit,
               'year' = 365.25,
               'month' = 30.44,
               'week' = 7,
               'day' = 1)
+
   d1 <- dob(cpr)
   d2 <- as.Date(date)
   a <- as.numeric(d2 - d1, units = 'days')
   a / u
 }
 
-dobone <- function(cpr) {
-  dd <- as.numeric(substr(cpr, 1, 2))
-  mm <- as.numeric(substr(cpr, 3, 4))
-  yy <- as.numeric(substr(cpr, 5, 6))
-  x7 <- as.numeric(substr(cpr, 7, 7))
+#' Gender
+#'
+#' Calculate genders from CPR numbers.
+#'
+#' @param cpr Character Vector of CPR numbers with or without hyphens.
+#'
+#' @return Integer vector of genders. 0 = Female, 1 = Male.
+#'
+#' @examples
+#'   gender(c('1508631111', '1310762222'))
+#'
+#' @export
+#'
+gender <- function(cpr) {
+  cpr <- clean(cpr)
 
-  if(x7 < 4) {
-    yyyy <- 1900
-  } else if(x7 == 4 & yy < 37) {
-    yyyy <- 2000
-  } else if(x7 == 4) {
-    yyyy <- 1900
-  } else if(x7 == 5 & yy < 58) {
-    yyyy <- 2000
-  } else if(x7 == 5) {
-    yyyy <- 1800
-  } else if(x7 == 6 & yy < 58) {
-    yyyy <- 2000
-  } else if(x7 == 6) {
-    yyyy <- 1800
-  } else if(x7 == 7 & yy < 58) {
-    yyyy <- 2000
-  } else if(x7 == 7) {
-    yyyy <- 1800
-  } else if(x7 == 8 & yy < 58) {
-    yyyy <- 2000
-  } else if(x7 == 8) {
-    yyyy <- 1800
-  } else if(x7 == 9 & yy < 37) {
-    yyyy <- 2000
-  } else {
-    yyyy <- 1900
+  as.numeric(substring(cpr, 10)) %% 2
+
+}
+
+#' Scramble
+#'
+#' Make "ananymous" random CPR numbers to random numbers.
+#'
+#' @param cpr Character Vector of CPR numbers with or without hyphens.
+#'
+#' @return A data frame with corresponding CPR and anonymous id numbers.
+#'
+#' @examples
+#'   scramble(c('1508631111', '1310762222'))
+#'
+#' @export
+#'
+scramble <- function(cpr) {
+  x <- seq_along(sample(unique(cpr)))
+  names(x) <- sample(unique(cpr))
+  x <- x[cpr]
+  names(x) <- NULL
+  x
+}
+
+clean <- function(cpr) {
+  cpr <- gsub('[^[:alnum:]]+', '', cpr)
+
+  if(any(nchar(cpr[!is.na(cpr)]) != 10)) {
+    stop('One or more invalid CPR numbers.')
   }
-
-  yyyy <- yyyy + yy
-
-  as.Date(paste(yyyy, mm, dd, sep = '-'))
+  cpr
 }
