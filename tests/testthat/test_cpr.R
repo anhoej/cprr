c9a7fe807f42cb56a5bfa36f9a7157a783aaef29
anhoej <jacob@anhoej.net>
2017-09-04 12:50:13 +0200

    Tests robustified.

--- tests/testthat/test_cpr.R
+++ tests/testthat/test_cpr.R
@@ -2,7 +2,7 @@ library(cprr)
 context('CPR validation')
 
 test_that('Date of birth is corretly calculated from test CPR numbers',
-          expect_identical(dob(test_cpr$cpr), test_cpr$dob))
+          expect_equal(dob(test_cpr$cpr), test_cpr$dob))
 
 test_that('Gender is corretly calculated from test CPR numbers',
-          expect_identical(gender(test_cpr$cpr), test_cpr$gender))
+          expect_equal(gender(test_cpr$cpr), test_cpr$gender))
